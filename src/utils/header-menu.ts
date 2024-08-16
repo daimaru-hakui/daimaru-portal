619524783f60904609ca41c2cb08abb31a03491c
daimaru-hakui <dh.mukai.dh@gmail.com>
2024-08-16 11:49:37 +0900

    catalog url

--- src/utils/header-menu.ts
+++ src/utils/header-menu.ts
@@ -1,41 +1,41 @@
 export const menu = [
-    {
-      title: "マイユニポータル",
-      link: "https://myuni.vercel.app/catalog",
-      blank: true,
-    },
-    {
-      title: "WEB在庫",
-      link: "https://daimaru-web-zaiko.vercel.app/",
-      blank: true,
-    },
-    {
-      title: "ホームページ",
-      link: "https://www.daimaru-hakui.co.jp/",
-      blank: true,
-    },
-    {
-      title: "組織カレンダー",
-      link: "https://calendar.nextset.jp/daimaruhakui/gp/calendar.html",
-      blank: true,
-    },
-    {
-      title: "生地在庫",
-      link: "https://daimaru-kijizaiko.vercel.app/dashboard",
-      blank: true,
-    },
-    {
-      title: "裁断報告書",
-      link: "https://daimaru-kijizaiko.vercel.app/tokushima/cutting-reports",
-      blank: true,
-    },
-    {
-      title: "売掛金情報",
-      link: "/receivables",
-    },
-    {
-      title: "会社カレンダー",
-      link: "/calendar",
-    },
-    { title: "メーカーWEB", link: "/makerweb" },
-  ];
+  {
+    title: "マイユニポータル",
+    link: "https://catalog.myuni.jp/",
+    blank: true,
+  },
+  {
+    title: "WEB在庫",
+    link: "https://daimaru-web-zaiko.vercel.app/",
+    blank: true,
+  },
+  {
+    title: "ホームページ",
+    link: "https://www.daimaru-hakui.co.jp/",
+    blank: true,
+  },
+  {
+    title: "組織カレンダー",
+    link: "https://calendar.nextset.jp/daimaruhakui/gp/calendar.html",
+    blank: true,
+  },
+  {
+    title: "生地在庫",
+    link: "https://daimaru-kijizaiko.vercel.app/dashboard",
+    blank: true,
+  },
+  {
+    title: "裁断報告書",
+    link: "https://daimaru-kijizaiko.vercel.app/tokushima/cutting-reports",
+    blank: true,
+  },
+  {
+    title: "売掛金情報",
+    link: "/receivables",
+  },
+  {
+    title: "会社カレンダー",
+    link: "/calendar",
+  },
+  { title: "メーカーWEB", link: "/makerweb" },
+];
